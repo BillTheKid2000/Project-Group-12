e8c5f52e1f5e4cd84aa54e7535e3415a8b2a5f87
EricPomerantz <epomerantz2021@fau.edu>
2023-07-18 00:44:15 -0400

    Skeleton Prototype
    Prototype with no styling but most functions working.

--- your-script.js
+++ your-script.js
@@ -1,42 +1,71 @@
 const apiKey = 'YOUR_API_KEY';
 const searchUrl = 'http://archive.org/wayback/available?url=';
 
-// Get the input values from the textboxes
-const inputTextbox1 = document.getElementById('urlInput1'); // Assuming you have an input element with the ID 'urlInput1'
-const inputTextbox2 = document.getElementById('urlInput2'); // Assuming you have an input element with the ID 'urlInput2'
+// Get the input value from the textbox
+const inputTextbox = document.getElementById('urlInput');
 
 // Get the display boxes
-const displayBox1 = document.getElementById('displayBox1'); // Assuming you have a div element with the ID 'displayBox1'
-const displayBox2 = document.getElementById('displayBox2'); // Assuming you have a div element with the ID 'displayBox2'
+const currentWebPageBox = document.getElementById('currentWebPage');
+const archivedWebPageBox = document.getElementById('archivedWebPage');
 
-// Function to handle the search and display for display box 1
-function searchAndDisplaySnapshot1() {
-  // Get the user input URL for display box 1
-  const targetUrl1 = inputTextbox1.value;
+// Function to handle the search and display
+function searchAndDisplayWebPages() {
+  // Get the user input URL
+  const targetUrl = inputTextbox.value;
 
-  // Create the request URL for display box 1
-  const requestUrl1 = `${searchUrl}${targetUrl1}`;
+  // Create the request URLs
+  const currentWebPageRequestUrl = `${searchUrl}${targetUrl}`;
+  const archivedWebPageRequestUrl = `${searchUrl}${targetUrl}&timestamp=${getTimestampTenYearsAgo()}`;
 
-  // Make the AJAX request to the Wayback Machine API for display box 1
+  // Make the AJAX request to the Wayback Machine API for the current web page
   $.ajax({
-    url: requestUrl1,
+    url: currentWebPageRequestUrl,
     method: 'GET',
     dataType: 'json',
     headers: {
       'Authorization': `LOW ${apiKey}`
     },
     success: function (response) {
-      // Check if the response contains archived snapshots for display box 1
+      // Check if the response contains an archived snapshot
       if (response && response.archived_snapshots && response.archived_snapshots.closest) {
         const closest = response.archived_snapshots.closest;
         const snapshotUrl = closest.url;
 
-        // Display the snapshot on display box 1
+        // Display the snapshot of the current web page
         const iframe = document.createElement('iframe');
         iframe.src = snapshotUrl;
-        displayBox1.appendChild(iframe);
+        currentWebPageBox.innerHTML = '';
+        currentWebPageBox.appendChild(iframe);
       } else {
-        console.log('No archived snapshots found for display box 1.');
+        currentWebPageBox.innerHTML = 'No archived snapshots found for the current web page.';
+      }
+    },
+    error: function (error) {
+      console.log('Error:', error);
+    }
+  });
+
+  // Make the AJAX request to the Wayback Machine API for the archived web page
+  $.ajax({
+    url: archivedWebPageRequestUrl,
+    method: 'GET',
+    dataType: 'json',
+    headers: {
+      'Authorization': `LOW ${apiKey}`
+    },
+    success: function (response) {
+      // Check if the response contains an archived snapshot
+      if (response && response.archived_snapshots && response.archived_snapshots.closest) {
+        const closest = response.archived_snapshots.closest;
+        const snapshotUrl = closest.url;
+
+        // Display the snapshot of the archived web page
+        const iframe = document.createElement('iframe');
+        iframe.src = snapshotUrl;
+        archivedWebPageBox.innerHTML = '';
+        archivedWebPageBox.appendChild(iframe);
+      } else {
+        archivedWebPageBox.innerHTML = 'No archived snapshots found for the archived web page.';
       }
     },
     error: function (error) {
@@ -45,45 +74,18 @@ function searchAndDisplaySnapshot1() {
   });
 }
 
-// Function to handle the search and display for display box 2
-function searchAndDisplaySnapshot2() {
-  // Get the user input URL for display box 2
-  const targetUrl2 = inputTextbox2.value;
-
-  // Create the request URL for display box 2
-  const requestUrl2 = `${searchUrl}${targetUrl2}`;
-
-  // Make the AJAX request to the Wayback Machine API for display box 2
-  $.ajax({
-    url: requestUrl2,
-    method: 'GET',
-    dataType: 'json',
-    headers: {
-      'Authorization': `LOW ${apiKey}`
-    },
-    success: function (response) {
-      // Check if the response contains archived snapshots for display box 2
-      if (response && response.archived_snapshots && response.archived_snapshots.closest) {
-        const closest = response.archived_snapshots.closest;
-        const snapshotUrl = closest.url;
-
-        // Display the snapshot on display box 2
-        const iframe = document.createElement('iframe');
-        iframe.src = snapshotUrl;
-        displayBox2.appendChild(iframe);
-      } else {
-        console.log('No archived snapshots found for display box 2.');
-      }
-    },
-    error: function (error) {
-      console.log('Error:', error);
-    }
-  });
+// Helper function to get the timestamp of the date 10 years ago in the format YYYYMMDD
+function getTimestampTenYearsAgo() {
+  const currentDate = new Date();
+  const tenYearsAgo = new Date();
+  tenYearsAgo.setFullYear(currentDate.getFullYear() - 10);
+  const year = tenYearsAgo.getFullYear();
+  const month = String(tenYearsAgo.getMonth() + 1).padStart(2, '0');
+  const day = String(tenYearsAgo.getDate()).padStart(2, '0');
+  return `${year}${month}${day}`;
 }
 
-// Attach event listeners to trigger the search and display for each display box
-const searchButton1 = document.getElementById('searchButton1'); // Assuming you have a button element with the ID 'searchButton1'
-const searchButton2 = document.getElementById('searchButton2'); // Assuming you have a button element with the ID 'searchButton2'
+// Attach an event listener to trigger the search and display when the button is clicked
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', searchAndDisplayWebPages);
 
-searchButton1.addEventListener('click', searchAndDisplaySnapshot1);
-searchButton2.addEventListener('click', searchAndDisplaySnapshot2);
