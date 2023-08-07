15d3a05d537dbdcf1fa5a9f0ba5571317abc79e2
EricPomerantz <128101665+EricPomerantz@users.noreply.github.com>
2023-08-06 21:08:21 -0400

    Update auth-1.js

--- assets/js/auth-1.js
+++ assets/js/auth-1.js
@@ -1,73 +1,59 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCBOu2NukPMhGXr46EO7aY8R-IIbReW31c",
-  authDomain: "memoria-web-51384.firebaseapp.com",
-  projectId: "memoria-web-51384",
-  storageBucket: "memoria-web-51384.appspot.com",
-  messagingSenderId: "887978552785",
-  appId: "1:887978552785:web:bb074f05fef446a45544f0",
-  measurementId: "G-S7EL6RSHGE"
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
-
-// Get the auth instance
-const auth = getAuth();
-
-// Create a Google provider object
-const provider = new GoogleAuthProvider();
-
-function signUp(email, password) {
-  createUserWithEmailAndPassword(auth, email, password).then(cred => {
-    console.log(cred.user);
-    alert(cred.user.email + ' has been registered');
-    closeModal('signupModal');
+function signUp (email, password) {
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+      alert(cred.user.email + ' has been registered');
+    // Close the signup modal
+      closeModal('signupModal');
+    // Clear the input fields
     document.getElementById('signup-email').value = '';
     document.getElementById('signup-password').value = '';
-  });
-}
-
-function login(email, password) {
-  signInWithEmailAndPassword(auth, email, password).then(cred => {
-    alert(cred.user.email + ' has logged in');
-    console.log(cred.user);
-    closeModal('signinModal');
+    });
+  }
+  
+  function login(email, password) {
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        alert(cred.user.email + ' has logged in');
+        console.log(cred.user);
+    // Close the signin modal
+      closeModal('signinModal');
+    // Clear the input fields
     document.getElementById('signin-email').value = '';
     document.getElementById('signin-password').value = '';
-  });
-}
-
-// Sign in with Google
-function signInWithGoogle() {
-  signInWithPopup(auth, provider).then((result) => {
-    alert(result.user.displayName + ' has logged in with Google');
-    console.log(result.user);
-  }).catch((error) => {
-    alert('Error logging in with Google: ' + error.message);
-    console.log(error);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+    });
+  }
+  
+  // Adding event listener for sign-up form submission
   document.getElementById('signup-form').addEventListener('submit', function(event) {
     event.preventDefault();
     var email = document.getElementById('signup-email').value;
     var password = document.getElementById('signup-password').value;
     signUp(email, password);
   });
-
+  
+  // Adding event listener for login form submission
   document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
     var email = document.getElementById('signin-email').value;
     var password = document.getElementById('signin-password').value;
     login(email, password);
   });
+  
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log("User is signed in with email:", user.email);
+      
+    } else {
+      console.log("User is signed out.");
+      
+    }
+  });
 
-  // Adding event listener for the Google login button
-  document.getElementById('google-login-button').addEventListener('click', signInWithGoogle);
-});
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  .then(() => {
+   
+  })
+  .catch((error) => {
+    // Handle errors here.
+    console.log(error.message);
+  });
+
